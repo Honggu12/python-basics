79c6b00f0d15e334f7f05966ace1abe54fb87dc2
honggu12 <gu4734@hanmail.net>
2020-08-26 17:32:37 +0900

    commit

--- 01/hello.py
+++ 01/hello.py
@@ -1,3 +1,25 @@
 # helloworld.py
 
+
+# 블록 시작(전역범위)
 print('hello world')
+a = 2
+if a > 1:     # 블록 시작(조건문)
+    b = 1
+    print(a)
+    print('big')
+       # 블록 끝(조건문)
+
+
+def my_func(i):
+    print('블록 시작(함수정의)')
+    if i > 10:
+        print('블록 시작(조건문)')
+        print('big')
+        print('...')
+        print('블록 끝(함수정의)')
+    else:
+        print('small')
+
+# 함수 호출
+my_func(20)
